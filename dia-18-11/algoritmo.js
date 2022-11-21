6a7d9e3249114f135e5d972e5a59eab450ce5779
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2022-11-20 23:59:40 -0300

    adicionando o restante do desafio

--- dia-18-11/algoritmo.js
+++ dia-18-11/algoritmo.js
@@ -1,14 +1,38 @@
-let i = 0
-
 let maiorValorAtual = 0
+let modeloMaiorValor = ''
+let anoMaiorValor = 0
 
-for(i = 0; i < 3; i++){
+let menorValorAtual = Infinity
+let modeloMenorValor = ''
+let anoMenorValor = 0
+
+const array = []
+
+for (let i = 0; i < 3; i++) {
     var modeloCarro = prompt('Qual é o modelo do seu carro?')
     var anoCarro = parseInt(prompt('Qual é o ano do carro?'))
     var valorCarro = parseInt(prompt('Qual é o valor do seu carro?'))
-    if(valorCarro > maiorValorAtual){
+    if (valorCarro > maiorValorAtual) {
         maiorValorAtual = valorCarro
+        modeloMaiorValor = modeloCarro
+        anoMaiorValor = anoCarro
     }
+    if (valorCarro < menorValorAtual) {
+        menorValorAtual = valorCarro
+        modeloMenorValor = modeloCarro
+        anoMenorValor = anoCarro
+    }
+    array.push(valorCarro)
 }
 
-console.log(maiorValorAtual)
+let soma = 0
+
+for (let i = 0; i < array.length; i++) {
+    soma += array[i]
+}
+
+let valorMedio = Math.floor(soma / 3)
+
+console.log(`Carro mais caro: ${modeloMaiorValor}, ${anoMaiorValor}, R$${maiorValorAtual}`)
+console.log(`Carro mais barato: ${modeloMenorValor}, ${anoMenorValor}, R$${menorValorAtual}`)
+console.log(`O valor médio dos carros é R$${valorMedio}`)
